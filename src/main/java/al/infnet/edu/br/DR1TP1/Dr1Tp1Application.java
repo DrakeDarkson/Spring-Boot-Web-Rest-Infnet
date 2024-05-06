240ea114eb1840cb5b679d6e1806e11ddd8363d4
DrakeDarkson <brenodssan@gmail.com>
2024-05-06 14:08:01 -0300

    first commit

---
+++ src/main/java/al/infnet/edu/br/DR1TP1/Dr1Tp1Application.java
@@ -0,0 +1,14 @@
+package al.infnet.edu.br.DR1TP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr1Tp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr1Tp1Application.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/al/infnet/edu/br/DR1TP1/Dr1Tp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package al.infnet.edu.br.DR1TP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dr1Tp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
